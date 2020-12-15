d4c4e06af4faad2bcdc96c6522907b8e75ca776b
dorinel pirvu <dorinel.pirvu@outlook.com>
2020-12-15 13:26:18 +0200

    final

--- BookStore-UI/Static/EndPoints.cs
+++ BookStore-UI/Static/EndPoints.cs
@@ -7,7 +7,14 @@ namespace BookStore_UI.Static
 {
     public static class EndPoints
     {
+#if DEBUG
         public static string BaseUrl = "https://localhost:44395/";
+
+#else
+        public static string BaseUrl = "";
+
+#endif 
+        
         public static string AuthorsEndPoint = $"{BaseUrl}api/authors/";
         public static string BooksEndPoint = $"{BaseUrl}api/Books/";
         public static string RegistersEndPoint = $"{BaseUrl}api/Users/register/";
